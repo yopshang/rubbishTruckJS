417f9ce3308dc379a5f53dfbcd71c75a92a65167
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2023-08-02 11:34:31 +0800

    fix: rebuild google map

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -13,67 +13,102 @@ const Marker = ({ text, lat, lng }) => {
   );
 }
 
-function Googlemap (){
-  const [currentLocation, setCurrentLocation] = useState(null);
-  const [mapCenter, setMapCenter] = useState({ lat: 25.0330, lng: 121.5654 })
+// function Googlemap (){
+//   const [currentLocation, setCurrentLocation] = useState(null);
+//   const [mapCenter, setMapCenter] = useState({ lat: 25.0330, lng: 121.5654 })
 
-  useEffect(()=>{
+//   useEffect(()=>{
   
-    // 使用 Geolocation API 取得目前位置
-    if(navigator.geolocation){ 
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-            const {latitude, longitude} = position.coords;
-            setCurrentLocation({lat: latitude, lng: longitude})
-            setMapCenter({lat: latitude, lng: longitude})
-            console.log('currentLocation', currentLocation, 'mapCenter', mapCenter)
-        },
-        (error) => {
-          console.log('取得目前位置失敗:', error.message)
-        }
-      )
-    } else {
-      console.log('瀏覽器不支援 Geolocation API')
-    }
-  }, [])
+//     // 使用 Geolocation API 取得目前位置
+//     if(navigator.geolocation){ 
+//       navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//             const {latitude, longitude} = position.coords;
+//             setCurrentLocation({lat: latitude, lng: longitude})
+//             setMapCenter({lat: latitude, lng: longitude})
+//             console.log('currentLocation', currentLocation, 'mapCenter', mapCenter)
+//         },
+//         (error) => {
+//           console.log('取得目前位置失敗:', error.message)
+//         }
+//       )
+//     } else {
+//       console.log('瀏覽器不支援 Geolocation API')
+//     }
+//   }, [])
 
 
+//   const reactkey = import.meta.env.VITE_APP_KEY;
+
+//   return (
+//     <div style={{ height: 'calc(100vh - 58px)', width: '96vw' }}>
+//       {currentLocation? (
+//       <GoogleMapReact
+//         bootstrapURLKeys={{key: reactkey}}
+//         defaultCenter={mapCenter}
+//         defaultZoom={14}
+//         onZoomAnimationStart={()=>{
+//           console.log('偵測到開始縮放')
+//         }}
+//         onZoomAnimationEnd={()=>{
+//           console.log('偵測到結束縮放')
+//         }}
+//       >
+//         {/* 在地圖上標記一個位置 */}
+//         <Marker
+//           lat={25.0511929}
+//           lng={121.5940662}
+//           text="這裡是一個標記"
+//         />
+
+//         {/* 在地圖上標記另一個位置 */}
+//         <Marker
+//           lat={25.0375}
+//           lng={121.5637}
+//           text="另一個標記位置"
+//         />
+//       </GoogleMapReact>
+
+//       ):(
+//         <div>等待取得目前位置...</div>
+//       )
+//     }
+//     </div>
+//   )}
+
+
+
+function Googlemap(){
+  const [mapCenter, setMapCenter] = useState(null)
   const reactkey = import.meta.env.VITE_APP_KEY;
 
+
+  // 取得目前位置
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      async position=>{
+        const {latitude, longitude} = position.coords;
+        // 將目前位置設為地圖中心
+        await setMapCenter({lat:latitude, lng:longitude})
+      },
+      error=>{
+        console.log('無法取得目前位置:', error.message)
+      })
+    },[])
+    console.log('mapCenter',mapCenter)
+ 
+
   return (
-    <div style={{ height: 'calc(100vh - 58px)', width: '96vw' }}>
-      {currentLocation? (
+    mapCenter===null?(<div>
+      資料載入中...
+    </div>):(<div style={{height: '100vh', width:'100%'}}>
       <GoogleMapReact
         bootstrapURLKeys={{key: reactkey}}
         defaultCenter={mapCenter}
         defaultZoom={14}
-        onZoomAnimationStart={()=>{
-          console.log('偵測到開始縮放')
-        }}
-        onZoomAnimationEnd={()=>{
-          console.log('偵測到結束縮放')
-        }}
-      >
-        {/* 在地圖上標記一個位置 */}
-        <Marker
-          lat={25.0511929}
-          lng={121.5940662}
-          text="這裡是一個標記"
-        />
-
-        {/* 在地圖上標記另一個位置 */}
-        <Marker
-          lat={25.0375}
-          lng={121.5637}
-          text="另一個標記位置"
-        />
-      </GoogleMapReact>
-
-      ):(
-        <div>等待取得目前位置...</div>
-      )
-    }
-    </div>
-  )}
+      ></GoogleMapReact>
+    </div>)
+  )
+}
 
 export default Googlemap
